017311a9f1ffc032921519065550d1e655b5d73e
imgautamm <imgautamm@gmail.com>
2025-01-22 00:51:23 +0100

    Update All

---
+++ Madlibsgame.py
@@ -0,0 +1,13 @@
+#Madlibs game
+
+adjactive1 = input("Enter an adjactive (description):")
+noun1 = input("Enter a noun (person, place, thing):")
+adjactive2 = input("Enter an adjective (description)")
+verb1 = input("Enter a verb ending with 'ing'")
+adjactive3 = input("Enter an adjactive (dexcription):")
+
+print(f"Today I went to a {adjactive1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjactive2} and {verb1}")
+print(f"I was {adjactive3}!")
+
