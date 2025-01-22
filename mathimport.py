dd538ea9d67e9002a6ce16720667ccea8d1d5373
imgautamm <imgautamm@gmail.com>
2025-01-22 01:21:47 +0100

    Update All

---
+++ mathimport.py
@@ -0,0 +1,7 @@
+import math
+
+radius = float(input("Enter the radius of circle:"))
+circumference = 2 * math.pi * radius
+
+print(f" The circumference of the circle is {round(circumference, 2)} cm")
+
